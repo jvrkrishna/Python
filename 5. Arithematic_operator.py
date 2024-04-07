d87a7314e1b25342b615a96a296c274908b571c1
Rama Krishna <jvrkrishnaa@gmail.com>
2024-04-07 16:29:14 +0530

    k

---
+++ 5. Arithematic_operator.py
@@ -0,0 +1,29 @@
+a=10
+b=20
+# Addition(+)
+add=a+b
+print(add)
+
+# Subtraction (-)
+sub=b-a
+print(sub)
+
+# Multiplication (*)
+mul=a*b
+print(mul)
+
+# Division (/)
+div=b/a
+print(div)
+
+# Floor Division (//) It does not give any decimal value.
+fdiv=b//a
+print(fdiv)
+
+# modulas (%)
+mod=b%a
+print(mod)
+
+# power/square (**)
+pow=a**b
+print(pow)
