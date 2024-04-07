21b0cf4ab823da5325511019d66f54c110057d9a
Rama Krishna <jvrkrishnaa@gmail.com>
2024-04-07 16:46:46 +0530

    k

---
+++ 5.3.Assignment_Operator.py
@@ -0,0 +1,17 @@
+x=10
+print("Assign the value to variable where x is:",x)
+
+x+= 3
+print(x)
+# a += b
+# a *= b
+# a/=b
+# a%=b
+# a -= b
+# a //= b
+# a **= b
+# a &= b
+# a |= b
+# a ^= b
+# a >>= b
+# a <<= b
