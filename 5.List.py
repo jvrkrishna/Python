1da2d7d65f79bdc78166c6485e49448fb3235538
Rama Krishna <jvrkrishnaa@gmail.com>
2024-04-17 16:54:47 +0530

    k

---
+++ 5.List.py
@@ -0,0 +1,27 @@
+L1 = ["John", 102, 'USA',10,"20"]
+L2 = [1, 2, 3, 4, 5, 6]
+L3=list([1,"Ram"])
+print(type(L1))
+print(type(L2))
+print(type(L3))
+print(L1)
+
+list = [1,2,3,4,5,6,7]
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+
+# Slicing the elements
+print(list[0:6])
+print(list[:])
+print(list[2:5])
+print(list[2:5+1])
+print(list[1:6:2])
+print(list[-1])
+
+list2 = [1,2,3,4,5]
+print(list2[-1])
+print(list2[-3:])
+print(list2[:-1])
+print(list2[-3:-1])
