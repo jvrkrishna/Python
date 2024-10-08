f46321d75d8d44a92307a8e5f610972859fdb31c
jvrkrishna <jvrkrishnaa@gmail.com>
2024-10-08 13:58:07 +0530

    s

--- 2.Data_Types/4.1.Number_data_type.py
+++ 2.Data_Types/4.1.Number_data_type.py
@@ -1,26 +1,26 @@
-a=10.7 #here we cannot delare data type because python is dynamically typed
-print(a)
-print(type(a))
-print(id(a))
+# a=10.7 #here we cannot delare data type because python is dynamically typed
+# print(a)
+# print(type(a))
+# print(id(a))
 
-a="Rama krishna"
-print(a)
-print(type(a))
-print(id(a))
+# a="Rama krishna"
+# print(a)
+# print(type(a))
+# print(id(a))
 
-x = 8 # int
-y = 2.8  # float
-z = 1j   # complex
-# To verify the type of any object in Python, use the type() function:
-# Example
-print(type(x))
-print(type(y))
-print(type(z))
+# x = 8 # int
+# y = 2.8  # float
+# z = 1j   # complex
+# # To verify the type of any object in Python, use the type() function:
+# # Example
+# print(type(x))
+# print(type(y))
+# print(type(z))
 
 
 x = 1 # int
 y = 2.8 # float
-z = 1j # complex
+z = 1j # complex z=10+16j 10 real part and 16 is imaginary part
 #convert from int to float:
 a = float(x)
 #convert from float to int:
@@ -35,16 +35,16 @@ print(type(b))
 print(type(c))
 
 
-import random
-n=random.randrange(1,10)
-print(n)
+# import random
+# n=random.randrange(1,10)
+# print(n)
 
-a=input("Enter the input:")
-print(a)
+# a=input("Enter the input:")
+# print(a)
 
-a=int(input("Enter the input:"))
-print(a)
+# a=int(input("Enter the input:"))
+# print(a)
 
-print(type(a))
+# print(type(a))
 
 
