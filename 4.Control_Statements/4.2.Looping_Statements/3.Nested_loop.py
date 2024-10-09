1a2b6ec496aa9d3c51b613c6b9520a7bd2bcb391
jvrkrishna <jvrkrishnaa@gmail.com>
2024-10-09 10:19:06 +0530

    s

--- 4.Control_Statements/4.2.Looping_Statements/3.Nested_loop.py
+++ 4.Control_Statements/4.2.Looping_Statements/3.Nested_loop.py
@@ -1,3 +1,22 @@
+# i=1
+# while(i<=3):
+#     print(i)
+#     j=1
+#     while(j<=3):
+#         print(j)
+#         j=j+1
+#     i=i+1
+    
+# i=1
+# while(i<=3):
+#     print("Ram")
+#     j=1
+#     while(j<=3):
+#         print("Seetha")
+#         j=j+1
+#     i=i+1
+
+
 # for i in range(1,11):
 #     for j in range(2,4):
 #         print(i*j,end = ' ')
@@ -20,13 +39,13 @@
 
 
 
-x = [1, 2]
-y = [4, 5]
-i = 0
-while i < len(x) :
-    j = 0
-    while j < len(y) :
-        print(x[i] , y[j])
-        j = j + 1
-    i = i + 1
+# x = [1, 2]
+# y = [4, 5]
+# i = 0
+# while i < len(x) :
+#     j = 0
+#     while j < len(y) :
+#         print(x[i] , y[j])
+#         j = j + 1
+#     i = i + 1
 
