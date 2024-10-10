d22c422309e9f878b743b06cc96118d033bd8211
jvrkrishna <jvrkrishnaa@gmail.com>
2024-10-10 09:15:05 +0530

    is

--- 5.1 List.py
+++ 5.1 List.py
@@ -1,9 +1,19 @@
+'''List a collection of elements with various data types. It is mutable mean we can change 
+the values after creation of list also. List with [] brackets.'''
+
+#Creation of a list
+L1=[]
 L1 = ["John", 102, 'USA',10,"20"]
 L2 = [1, 2, 3, 4, 5, 6]
 L3=list([1,"Ram"])
 print(type(L1))
 print(type(L2))
 print(type(L3))
+print(L1)
+
+#Creation of list Dynamically by using eval() function.
+L1=eval(input("Enter the L1 Elements:"))  #input ------ [10,20,30] ---- brackets compulsary
+print(type(L1))
 print(L1)
 
 list = [1,2,3,4,5,6,7]
