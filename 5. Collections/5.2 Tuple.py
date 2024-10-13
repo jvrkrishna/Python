4971f05e25dfde64e6ea7a05c8eea642ba7b7281
jvrkrishna <jvrkrishnaa@gmail.com>
2024-10-13 20:48:07 +0530

    Tuple Completed

--- 5. Collections/5.2 Tuple.py
+++ 5. Collections/5.2 Tuple.py
@@ -53,3 +53,86 @@ print(my_tuple[-6])	# 'p'
 
 #Access the tuple by using slicing
 print(my_tuple[::])
+
+######## Changing tuple element ################
+my_tuple = (4, 2, 3, [6, 5])
+my_tuple[3][0] = 9  # Output: (4, 2, 3, [9, 5]) 
+
+#my_tuple[2]=4 Raise an error because tuple is immutable.
+print(my_tuple)
+my_tuple = ('p', 'r', 'o', 'g', 'r', 'a', 'm', 'i', 'z') 
+print(my_tuple)
+
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+print(x)
+
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.append("orange")
+thistuple = tuple(y)
+print(thistuple)
+
+'''We can use + operator to combine two tuples. This is called concatenation.
+We can also repeat the elements in a tuple for a given number of times using the * operator.
+Both + and * operations result in a new tuple.'''
+
+# Concatenation
+# Output: (1, 2, 3, 4, 5, 6)
+print((1, 2, 3) + (4, 5, 6))
+
+# Repeat
+# Output: ('Repeat', 'Repeat', 'Repeat') 
+print(("Repeat",) * 3)
+
+# traverse using for loop
+t=(10,20,30,40,50)
+for i in t:
+    print(i)
+    
+# traverse using while loop
+t=(10,20,30,40,50)
+i=0
+while i<len(t):
+    print(t[i])
+    i=i+1
+    
+# Deleting tuples
+my_tuple = ('p', 'r', 'o', 'g', 'r', 'a', 'm', 'i', 'z') # can't delete items
+
+# TypeError: 'tuple' object doesn't support item deletion 
+del my_tuple[3]
+
+# Can delete an entire tuple 
+del my_tuple
+
+# NameError: name 'my_tuple' is not defined
+print(my_tuple)
+
+######### In built tuple methods ###########
+# cmp(tuple1,tuple2)
+# len(tuple)
+# max(tuple)
+# min(tuple)
+# tuple(seq)
+
+####### packing in tuple ###########
+a=10
+b=20
+c=30
+d=40
+e="ram"
+pack=a,b,c,d,e
+print(pack)
+print(type(pack))
+
+####### un-packing in tuple ###########
+pack=("Ram",10,20,30,40)
+a,b,c,d,e=pack
+print(a,b,c,d,e) #at the time of un-pack the no of variables and number of value must be same.
+
+##### Tuple not support comprehension
