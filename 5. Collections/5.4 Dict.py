7ffba180566dcd9518a39fd14d79106208516cf4
jvrkrishna <jvrkrishnaa@gmail.com>
2024-11-20 21:32:27 +0530

    dict

--- 5. Collections/5.4 Dict.py
+++ 5. Collections/5.4 Dict.py
@@ -27,3 +27,64 @@ print(my_dict)
 # from sequence having each item as a pair 
 my_dict = dict([(1,'apple'), (2,'ball')])
 print(my_dict)
+
+#Accessing the dict elements
+# get vs [] for retrieving elements 
+my_dict = {'name': 'Jack', 'age': 26} 
+
+# Output: Jack 
+print(my_dict['name'])
+
+# Output: 26 
+print(my_dict.get('age'))
+
+# Trying to access keys which doesn't exist throws error # Output None
+print(my_dict.get('address')) 
+
+# KeyError 
+print(my_dict['address'])
+
+#Check whether key is existed or not
+dict={1:"Rama",2:"Krishna"}
+print(2 in dict)
+print(3 not in dict)
+
+#Create the dictionary Dynamically
+dict={}
+
+while True:
+    key=input("Enter the Key :")
+    value=input("Enter the value :")
+    dict[key]=value
+    
+    choice=input("Enter the choice you want to add more elements in to dictionary [Y/N] :")
+    
+    if choice=='N':
+        break
+
+print(dict)
+
+#Traversing List
+dict={1:"Rama",2:"Krishna"}
+
+for i in dict:
+    print(i, dict[i])
+
+#Add and Modify
+dict={1:"Rama",2:"Krishna"}
+
+dict[3]="Kat"
+print(dict)
+
+dict[1]="Ram"  #modify
+print(dict)
+
+#Delete
+dict={1:"Rama",2:"Krishna"}
+
+del dict[1]
+print(dict)
+
+del dict   #entire dictionary deleted 
+print(dict)
+
