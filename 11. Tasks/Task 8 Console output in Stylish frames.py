36eb5c0b5256f984f547a7cbe65aeab0d09ed157
jvrkrishna <jvrkrishnaa@gmail.com>
2024-12-14 07:54:24 +0530

    ihyt

---
+++ 11. Tasks/Task 8 Console output in Stylish frames.py
@@ -0,0 +1,12 @@
+#Console output in stylish frame
+'''To put the console output in stylish frame we have to go through "unicode box drawings"
+By using the characters we can create fancy looking borders for the frames and tables.'''
+
+#To draw the frame choose our border style(single, double, dash, border thichness.)
+
+vertical='\u2551'
+horizontal='\u2550'
+top_left='\u2554'
+top_right='\u2557'
+bottom_left='\u255a'
+bottom_right='\u255d'
