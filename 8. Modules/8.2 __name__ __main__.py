9c6e5e067096f07310591dedffa034cd87b800e5
jvrkrishna <jvrkrishnaa@gmail.com>
2024-12-18 15:09:59 +0530

    hgk

--- 8. Modules/8.2 __name__ __main__.py
+++ 8. Modules/8.2 __name__ __main__.py
@@ -1,6 +1,6 @@
 '''
 1. __name__ is a special variable.
-2. If we want to know whether the program executed as a noramal individual program or as a module then we use __name__ variable.
+2. If we want to know whether the program executed as a normal individual program or as a module then we use __name__ variable.
 3. If you will execute a python individual program then it will give you the output as __main__.
 4. If you will execute the program as a module from some other program then the value of __name__ variable is name of the module.
 '''
