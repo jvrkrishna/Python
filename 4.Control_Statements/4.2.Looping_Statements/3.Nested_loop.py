0953db99e4d45193fd8c6d7fd8d0119ec7e4744b
jvrkrishna <jvrkrishnaa@gmail.com>
2024-12-20 21:06:54 +0530

    udsgfv

--- 4.Control_Statements/4.2.Looping_Statements/3.Nested_loop.py
+++ 4.Control_Statements/4.2.Looping_Statements/3.Nested_loop.py
@@ -50,6 +50,20 @@
 #     for y in fruits:
 #         print(x, y)
 
+#WAP to print the numbers as below output format
+'''
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1 
+'''
+
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
 
 
 
