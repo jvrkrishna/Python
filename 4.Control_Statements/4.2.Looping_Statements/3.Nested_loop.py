5195ea449157f227b53d959d3ee47a39cd5cd6c1
jvrkrishna <jvrkrishnaa@gmail.com>
2024-12-28 21:48:01 +0530

    pattern

--- 4.Control_Statements/4.2.Looping_Statements/3.Nested_loop.py
+++ 4.Control_Statements/4.2.Looping_Statements/3.Nested_loop.py
@@ -52,6 +52,20 @@
 
 #WAP to print the numbers as below output format
 '''
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5  
+'''
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+    
+#WAP to print the numbers as below output format
+'''
 1 2 3 4 5 
 1 2 3 4 
 1 2 3 
@@ -65,37 +79,6 @@ for i in range(5,0,-1):
     print()
     
     
-#########WAP to print uppere half diamond
-'''
-     1 
-    1 2 
-   1 2 3 
-  1 2 3 4 
- 1 2 3 4 5 
-'''
-
-#Example:
-n=6
-for i in range(1,n):
-    print(" "*(n-i),end="")
-    for j in range(1,i+1):
-        print(j,end=" ")
-    print()
-
-#WAP to print Lower diamond pattern
-'''
- 1 2 3 4 
-  1 2 3 
-   1 2 
-    1 
-'''
-n=6
-for i in range(n-2,0,-1):
-    print(" "*(n-i),end="") #print leading spaces
-    for j in range(1,i+1):
-        print(j,end=" ") #print numbers with spaces
-    print()
-
 
 
 
