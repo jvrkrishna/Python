6bb0b0acebe908636667b3b86cf6ca4b8b8b6c65
jvrkrishna <jvrkrishnaa@gmail.com>
2025-01-23 07:30:26 +0530

    Interface

---
+++ 12. OOP/19. Abstraction/3. Interface.py
@@ -0,0 +1,5 @@
+########## Interface ###############
+'''Interface If the class holds only abstract '''
+#A normal class will hold all the concrete methods.
+#An abstract class will hold both concrete and abstract methods.
+#An interface will only hold abstract methods.
