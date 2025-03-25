e55789c0cff17bff49dd48b478a94b2c2784a49f
jvrkrishna <jvrkrishnaa@gmail.com>
2025-03-25 10:30:24 +0530

    doneee

---
+++ sample1.py
@@ -0,0 +1,9 @@
+a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+c=int(input("Enter c value:"))
+if a>b & a>c:
+    print("a is big")
+elif b>c:
+    print("b is big")
+else:
+    print("c is big")
