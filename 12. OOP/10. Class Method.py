dbd26c8db53e4408792fccf416436f94fc6f94db
jvrkrishna <jvrkrishnaa@gmail.com>
2025-04-08 17:30:13 +0530

    sdd

--- 12. OOP/10. Class Method.py
+++ 12. OOP/10. Class Method.py
@@ -30,12 +30,17 @@ class person:
     def cls_method(cls):
         print(f"My school name is {cls.school_name}")
         person.another_cls_method()
+        cls.another_cls_method()
         
     @classmethod
     def another_cls_method(cls):
         print(f"My school name is {cls.school_name}")
         
 person.cls_method()
+
+s1=person()
+s1.cls_method()
+s1.another_cls_method()
 
 
 #Example 3: By using class method we can perform several operation to static variables.
