815a644eeaf2f63421f6b95115ae5eecc9bb407e
jvrkrishna <jvrkrishnaa@gmail.com>
2025-04-18 11:04:30 +0530

    doneee

--- 4.Control_Statements/4.1.Conditional_statements/1.if.py
+++ 4.Control_Statements/4.1.Conditional_statements/1.if.py
@@ -6,11 +6,11 @@ a=int(input("Enter a value:"))
 b=int(input("Enter b value:"))
 c=int(input("Enter c value:"))
 # Here checks all the conditions
-if a>b & a>c:
+if a>b and a>c:
     print("a is big")
     
-if b>c & b>a:
+if b>c and b>a:
     print("b is big")
     
-if c>b & c>a:
+if c>b and c>a:
     print("c is big")
