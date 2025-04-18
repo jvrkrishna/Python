db67e1ad085eb75db2ceb971e9d86bb94a668f9a
jvrkrishna <jvrkrishnaa@gmail.com>
2025-04-18 10:38:57 +0530

    donenene

---
+++ sample2.py
@@ -0,0 +1,7 @@
+name="Rama krishna"
+age=30
+
+print("My name is",name,"My age is",age)
+print("My name is %s My age is %d"%(name,age))
+print("My name is {} My age is {}".format(name,age))
+print(f"My name is {name} My age is {age}")
