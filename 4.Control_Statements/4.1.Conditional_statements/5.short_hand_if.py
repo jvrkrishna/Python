22a7f03d859cc2e7361422ceb0ced831c1ef10b9
jvrkrishna <jvrkrishnaa@gmail.com>
2025-04-22 10:34:17 +0530

    done

--- 4.Control_Statements/4.1.Conditional_statements/5.short_hand_if.py
+++ 4.Control_Statements/4.1.Conditional_statements/5.short_hand_if.py
@@ -1,3 +1,5 @@
 a=20
 b=10
 if a>b: print("a is greater than b")
+
+
--- 4.Control_Statements/4.1.Conditional_statements/6.short_hand_if_else.py
+++ 4.Control_Statements/4.1.Conditional_statements/6.short_hand_if_else.py
@@ -6,3 +6,5 @@ print("A") if a > b else print("B")
 a = 330
 b = 330
 print("A") if a > b else print("=") if a == b else print("B")
+
+
